58333da1db1952528c5ce7c78c62a787c73c9773
s.paulmichl <s.paulmichl@felder-group.com>
2025-11-28 12:41:18 +0100

    :sparkles: changed to web hash history

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,12 +6,12 @@
 
 import { setupLayouts } from 'virtual:generated-layouts'
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 import { useAuthStore } from '@/stores/auth.ts'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: setupLayouts(routes),
 })
 
